4132d94182e4d1b357b6d583d7e8e9e24437eb21
Chris Moesel <cmoesel@mitre.org>
2018-04-04 16:21:48 -0400

    Fix bad syntax on require statements
    Each require statement had a comma after it, making some very funky cqlforbrowsers.js.  It's kind of amazing it ever worked at all.

--- src/example/browser/cql4browsers.js
+++ src/example/browser/cql4browsers.js
@@ -3574,7 +3574,51 @@
 (function() {
   var aggregate, arithmetic, clinical, comparison, conditional, datetime, declaration, element, expression, external, i, instance, interval, j, len, len1, lib, libs, list, literal, logical, nullological, overloaded, parameters, quantity, query, ref, reusable, string, structured, type;
 
-  expression = require('./expression', aggregate = require('./aggregate', arithmetic = require('./arithmetic', clinical = require('./clinical', comparison = require('./comparison', conditional = require('./conditional', datetime = require('./datetime', declaration = require('./declaration', external = require('./external', instance = require('./instance', interval = require('./interval', list = require('./list', literal = require('./literal', logical = require('./logical', nullological = require('./nullological', parameters = require('./parameters', quantity = require('./quantity', query = require('./query', reusable = require('./reusable', string = require('./string', structured = require('./structured', type = require('./type', overloaded = require('./overloaded')))))))))))))))))))))));
+  expression = require('./expression');
+
+  aggregate = require('./aggregate');
+
+  arithmetic = require('./arithmetic');
+
+  clinical = require('./clinical');
+
+  comparison = require('./comparison');
+
+  conditional = require('./conditional');
+
+  datetime = require('./datetime');
+
+  declaration = require('./declaration');
+
+  external = require('./external');
+
+  instance = require('./instance');
+
+  interval = require('./interval');
+
+  list = require('./list');
+
+  literal = require('./literal');
+
+  logical = require('./logical');
+
+  nullological = require('./nullological');
+
+  parameters = require('./parameters');
+
+  quantity = require('./quantity');
+
+  query = require('./query');
+
+  reusable = require('./reusable');
+
+  string = require('./string');
+
+  structured = require('./structured');
+
+  type = require('./type');
+
+  overloaded = require('./overloaded');
 
   libs = [expression, aggregate, arithmetic, clinical, comparison, conditional, datetime, declaration, external, instance, interval, list, literal, logical, nullological, parameters, query, quantity, reusable, string, structured, type, overloaded];
 
