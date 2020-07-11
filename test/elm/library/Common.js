3540da965e96da9ee7a18afeb7a4af35d19f47aa
Chris Moesel <cmoesel@mitre.org>
2020-07-11 11:47:42 -0400

    Change test data from coffee to js
    Change all the data.coffee files to data.js and change the generator to generate them that way.  This makes tests go way faster and sets us up better for coffee-to-js conversion.

---
+++ test/elm/library/Common.js
@@ -0,0 +1,10 @@
+/*
+   WARNING: This is a GENERATED file.  Do not manually edit!
+
+   To generate this file:
+       - Edit Common.cql to add a CQL Snippet
+       - From java dir: ./gradlew :cql-to-elm:generateTestData
+*/
+
+/* eslint-disable */
+
