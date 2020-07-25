1e519e5d6ff20c8ae78469b2d4dd417c4c66ce13
Chris Moesel <cmoesel@mitre.org>
2020-07-25 19:12:44 -0400

    Update top-level code files to remove less-than-ideal decaffeinate artifacts

--- src/cql-datatypes.js
+++
@@ -1,7 +0,0 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-const DT = require('./datatypes/datatypes');
-
-for (let element of Object.keys(DT)) {
-  module.exports[element] = DT[element];
-}
