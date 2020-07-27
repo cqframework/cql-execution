46efb0cdbbc11444cb13b03d680956eb3b7e6744
Chris Moesel <cmoesel@mitre.org>
2020-07-27 10:29:44 -0400

    Update util files to remove less-than-ideal decaffeinate artifacts
    Also update cql4browsers, which I forgot the last few times.

--- src/util/comparison.js
+++ src/util/comparison.js
@@ -1,128 +1,118 @@
-/* eslint-disable
-    no-unreachable,
-    no-unused-vars,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS205: Consider reworking code to avoid use of IIFEs
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-let equals, equivalent;
-const { DateTime } = require('../datatypes/datetime');
 const { Uncertainty } = require('../datatypes/uncertainty');
 
-const areNumbers = (a, b) => (typeof a === 'number') && (typeof b === 'number');
+function areNumbers(a, b) {
+  return typeof a === 'number' && typeof b === 'number';
+}
 
-const areDateTimesOrQuantities = (a, b) => ((a != null ? a.isDateTime : undefined) && (b != null ? b.isDateTime : undefined)) ||
-((a != null ? a.isDate : undefined) && (b != null ? b.isDate : undefined)) ||
-((a != null ? a.isTime : undefined) && (b != null ? b.isTime : undefined)) ||
-((a != null ? a.isQuantity : undefined) && (b != null ? b.isQuantity : undefined));
+function areDateTimesOrQuantities(a, b) {
+  return ((a && a.isDateTime) && (b && b.isDateTime)) ||
+    ((a && a.isDate) && (b && b.isDate)) ||
+    ((a && a.isTime) && (b && b.isTime)) ||
+    ((a && a.isQuantity) && (b && b.isQuantity));
+}
 
-const isUncertainty = x => x instanceof Uncertainty;
+function isUncertainty(x) {
+  return x instanceof Uncertainty;
+}
 
-module.exports.lessThan = function(a, b, precision) {
-  switch (false) {
-    case !areNumbers(a, b): return a < b;
-    case !areDateTimesOrQuantities(a, b): return a.before(b, precision);
-    case !isUncertainty(a): return a.lessThan(b);
-    case !isUncertainty(b): return Uncertainty.from(a).lessThan(b);
-    default: return null;
-  }
-};
+function lessThan(a, b, precision) {
+  if (areNumbers(a, b)) { return a < b;
+  } else if (areDateTimesOrQuantities(a, b)) { return a.before(b, precision);
+  } else if (isUncertainty(a)) { return a.lessThan(b);
+  } else if (isUncertainty(b)) { return Uncertainty.from(a).lessThan(b);
+  } else { return null; }
+}
 
-module.exports.lessThanOrEquals = function(a, b, precision) {
-  switch (false) {
-    case !areNumbers(a, b): return a <= b;
-    case !areDateTimesOrQuantities(a, b): return a.sameOrBefore(b, precision);
-    case !isUncertainty(a): return a.lessThanOrEquals(b);
-    case !isUncertainty(b): return Uncertainty.from(a).lessThanOrEquals(b);
-    default: return null;
-  }
-};
+function lessThanOrEquals(a, b, precision) {
+  if (areNumbers(a, b)) { return a <= b;
+  } else if (areDateTimesOrQuantities(a, b)) { return a.sameOrBefore(b, precision);
+  } else if (isUncertainty(a)) { return a.lessThanOrEquals(b);
+  } else if (isUncertainty(b)) { return Uncertainty.from(a).lessThanOrEquals(b);
+  } else { return null; }
+}
 
-module.exports.greaterThan = function(a, b, precision) {
-  switch (false) {
-    case !areNumbers(a, b): return a > b;
-    case !areDateTimesOrQuantities(a, b): return a.after(b, precision);
-    case !isUncertainty(a): return a.greaterThan(b);
-    case !isUncertainty(b): return Uncertainty.from(a).greaterThan(b);
-    default: return null;
-  }
-};
+function greaterThan(a, b, precision) {
+  if (areNumbers(a, b)) { return a > b;
+  } else if (areDateTimesOrQuantities(a, b)) { return a.after(b, precision);
+  } else if (isUncertainty(a)) { return a.greaterThan(b);
+  } else if (isUncertainty(b)) { return Uncertainty.from(a).greaterThan(b);
+  } else { return null; }
+}
 
-module.exports.greaterThanOrEquals = function(a, b, precision) {
-  switch (false) {
-    case !areNumbers(a, b): return a >= b;
-    case !areDateTimesOrQuantities(a, b): return a.sameOrAfter(b, precision);
-    case !isUncertainty(a): return a.greaterThanOrEquals(b);
-    case !isUncertainty(b): return Uncertainty.from(a).greaterThanOrEquals(b);
-    default: return null;
-  }
-};
+function greaterThanOrEquals(a, b, precision) {
+  if (areNumbers(a, b)) { return a >= b;
+  } else if (areDateTimesOrQuantities(a, b)) { return a.sameOrAfter(b, precision);
+  } else if (isUncertainty(a)) { return a.greaterThanOrEquals(b);
+  } else if (isUncertainty(b)) { return Uncertainty.from(a).greaterThanOrEquals(b);
+  } else { return null; }
+}
 
-module.exports.equivalent = (equivalent = function(a, b) {
-  if ((a == null) && (b == null)) { return true; }
-  if ((a == null) || (b == null)) { return false; }
+function equivalent(a, b) {
+  if (a == null && b == null) { return true; }
+  if (a == null || b == null) { return false; }
 
   if (isCode(a)) { return codesAreEquivalent(a, b); }
 
   // Use overloaded 'equivalent' function if it is available
   if (typeof a.equivalent === 'function') { return a.equivalent(b); }
 
-  const [aClass, bClass] = Array.from(getClassOfObjects(a, b));
+  const [aClass, bClass] = getClassOfObjects(a, b);
 
   switch (aClass) {
     case '[object Array]':
       return compareEveryItemInArrays(a, b, equivalent);
-      break;
     case '[object Object]':
       return compareObjects(a, b, equivalent);
-      break;
     case '[object String]':
-    // Make sure b is also a string
+      // Make sure b is also a string
       if (bClass === '[object String]') {
-      // String equivalence is case- and locale insensitive
+        // String equivalence is case- and locale insensitive
         a = a.replace(/\s/g, ' ');
         b = b.replace(/\s/g, ' ');
-        return (a.localeCompare(b, 'en', {sensitivity: 'base'})) === 0;
+        return a.localeCompare(b, 'en', {sensitivity: 'base'}) === 0;
       }
       break;
   }
 
   return equals(a, b);
-});
+}
 
-var isCode = object => object.hasMatch && (typeof object.hasMatch === 'function');
+function isCode(object) {
+  return object.hasMatch && typeof object.hasMatch === 'function';
+}
 
-var codesAreEquivalent = (code1, code2) => code1.hasMatch(code2);
+function codesAreEquivalent(code1, code2) {
+  return code1.hasMatch(code2);
+}
 
-var getClassOfObjects = (object1, object2) => [object1, object2].map((obj) => ({}.toString.call(obj)));
+function getClassOfObjects(object1, object2) {
+  return [object1, object2].map((obj) => ({}.toString.call(obj)));
+}
 
-var compareEveryItemInArrays = (array1, array2, comparisonFunction) => (array1.length === array2.length) && array1.every((item, i) => comparisonFunction(item, array2[i]));
+function compareEveryItemInArrays(array1, array2, comparisonFunction) {
+  return array1.length === array2.length && array1.every((item, i) => comparisonFunction(item, array2[i]));
+}
 
-var compareObjects = function(a, b, comparisonFunction) {
+function compareObjects(a, b, comparisonFunction) {
   if (!classesEqual(a, b)) { return false; }
   return deepCompareKeysAndValues(a, b, comparisonFunction);
-};
+}
 
-var classesEqual = (object1, object2) => object2 instanceof object1.constructor && object1 instanceof object2.constructor;
+function classesEqual(object1, object2) {
+  return object2 instanceof object1.constructor && object1 instanceof object2.constructor;
+}
 
-var deepCompareKeysAndValues = function(a, b, comparisonFunction) {
+function deepCompareKeysAndValues(a, b, comparisonFunction) {
   let finalComparisonResult;
   const aKeys = getKeysFromObject(a).sort();
   const bKeys = getKeysFromObject(b).sort();
   // Array.every() will only return true or false, so set a flag for if we should return null
   let shouldReturnNull = false;
   // Check if both arrays of keys are the same length and key names match
-  if ((aKeys.length === bKeys.length) && aKeys.every((value, index) => value === bKeys[index])) {
-    finalComparisonResult = aKeys.every(function(key) {
+  if (aKeys.length === bKeys.length && aKeys.every((value, index) => value === bKeys[index])) {
+    finalComparisonResult = aKeys.every((key) => {
       // if both are null we should return true to satisfy ignoring empty values in tuples
-      if ((a[key] == null) && (b[key] == null)) { return true; }
+      if (a[key] == null && b[key] == null) { return true; }
       const comparisonResult = comparisonFunction(a[key], b[key]);
       if (comparisonResult === null) { shouldReturnNull = true; }
       return comparisonResult;
@@ -133,31 +123,25 @@ var deepCompareKeysAndValues = function(a, b, comparisonFunction) {
 
   if (shouldReturnNull) { return null; }
   return finalComparisonResult;
-};
+}
 
-var getKeysFromObject = function(object) {
-  let key;
-  const objectClass = {}.toString.call(object);
-  return !isFunction(key) ? (() => {
-    const result = [];
-    for (key in object) {
-      result.push(key);
-    }
-    return result;
-  })() : undefined;
-};
+function getKeysFromObject(object) {
+  return Object.keys(object).filter(k => !isFunction(object[k]));
+}
 
-var isFunction = input => input instanceof Function || ({}.toString.call(input) === '[object Function]');
+function isFunction(input) {
+  return input instanceof Function || {}.toString.call(input) === '[object Function]';
+}
 
-module.exports.equals = (equals = function(a, b) {
+function equals(a, b) {
   // Handle null cases first: spec says if either is null, return null
-  if ((a == null) || (b == null)) { return null; }
+  if (a == null || b == null) { return null; }
 
   // If one is a Quantity, use the Quantity equals function
-  if (a != null ? a.isQuantity : undefined) { return a.equals(b); }
+  if (a && a.isQuantity) { return a.equals(b); }
 
   // If one is a Ratio, use the ratio equals function
-  if (a != null ? a.isRatio : undefined) { return a.equals(b); }
+  if (a && a.isRatio) { return a.equals(b); }
 
   // If one is an Uncertainty, convert the other to an Uncertainty
   if (a instanceof Uncertainty) { b = Uncertainty.from(b);
@@ -167,35 +151,32 @@ module.exports.equals = (equals = function(a, b) {
   if (typeof a.equals === 'function') { return a.equals(b); }
 
   // Return true of the objects are primitives and are strictly equal
-  if (((typeof a === typeof b) && (typeof a === 'string')) || (typeof a === 'number') || (typeof a === 'boolean')) {
+  if (typeof a === typeof b && typeof a === 'string' || typeof a === 'number' || typeof a === 'boolean') {
     return a === b;
   }
 
   // Return false if they are instances of different classes
-  const [aClass, bClass] = Array.from(getClassOfObjects(a, b));
+  const [aClass, bClass] = getClassOfObjects(a, b);
   if (aClass !== bClass) { return false; }
 
   switch (aClass) {
     case '[object Date]':
     // Compare the ms since epoch
       return a.getTime() === b.getTime();
-      break;
     case '[object RegExp]':
     // Compare the components of the regular expression
       return ['source', 'global', 'ignoreCase', 'multiline'].every(p => a[p] === b[p]);
-      break;
     case '[object Array]':
-      if ((a.indexOf(null) >= 0) || (a.indexOf(undefined) >= 0) || (b.indexOf(null) >= 0) || (b.indexOf(undefined) >= 0)) { return null; }
+      if (a.indexOf(null) >= 0 || a.indexOf(undefined) >= 0 || b.indexOf(null) >= 0 || b.indexOf(undefined) >= 0) { return null; }
       return compareEveryItemInArrays(a, b, equals);
-      break;
     case '[object Object]':
       return compareObjects(a, b, equals);
-      break;
     case '[object Function]':
       return a.toString() === b.toString();
-      break;
   }
 
   // If we made it this far, we can't handle it
   return false;
-});
+}
+
+module.exports = { lessThan, lessThanOrEquals, greaterThan, greaterThanOrEquals, equivalent, equals };
--- src/util/util.js
+++ src/util/util.js
@@ -1,50 +1,43 @@
-/* eslint-disable
-    no-unused-vars,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS103: Rewrite code to no longer use __guard__
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-let getTimezoneSeparatorFromString, normalizeMillisecondsField, normalizeMillisecondsFieldInString, typeIsArray;
-module.exports.removeNulls = things => things.filter(x => x != null);
+function removeNulls(things) {
+  return things.filter(x => x != null);
+}
 
-module.exports.numerical_sort = (things, direction='asc') => things.sort(function(a,b){
-  if (direction === 'asc') {
-    return a - b;
-  } else {
-    return b - a;
-  }
-});
+function numerical_sort(things, direction='asc') {
+  return things.sort((a,b) => {
+    if (direction === 'asc') {
+      return a - b;
+    } else {
+      return b - a;
+    }
+  });
+}
 
-module.exports.isNull = value => value===null;
-  
-module.exports.typeIsArray  = (typeIsArray  = Array.isArray || (value => ({}).toString.call( value ) === '[object Array]'));
+function isNull(value) {
+  return value === null;
+}
 
-module.exports.allTrue = function(things) {
+const typeIsArray  = Array.isArray || (value => ({}).toString.call( value ) === '[object Array]');
+
+function allTrue(things) {
   if (typeIsArray(things)) {
     return things.every(x => x);
   } else {
     return things;
   }
-};
+}
 
-module.exports.anyTrue = function(things) {
+function anyTrue(things) {
   if (typeIsArray(things)) {
     return things.some(x => x);
   } else {
     return things;
   }
-};
+}
 
 //The export below is to make it easier if js Date is overwritten with CQL Date
-module.exports.jsDate = Date;
-module.exports.normalizeMillisecondsFieldInString = (normalizeMillisecondsFieldInString = function(string, msString) {
+const jsDate = Date;
+
+function normalizeMillisecondsFieldInString(string, msString) {
   // TODO: verify we are only removing numeral digits
   let timezoneField;
   msString = normalizeMillisecondsField(msString);
@@ -52,24 +45,27 @@ module.exports.normalizeMillisecondsFieldInString = (normalizeMillisecondsFieldI
   const timezoneSeparator = getTimezoneSeparatorFromString(msAndAfter);
 
   if (timezoneSeparator) { timezoneField = msAndAfter != null ? msAndAfter.split(timezoneSeparator)[1] : undefined; }
-  if ((timezoneField == null)) { timezoneField = ''; }
+  if (timezoneField == null) { timezoneField = ''; }
   return string = beforeMs + '.' + msString + timezoneSeparator + timezoneField;
-});
-
-module.exports.normalizeMillisecondsField = (normalizeMillisecondsField = msString => // fix up milliseconds by padding zeros and/or truncating (5 --> 500, 50 --> 500, 54321 --> 543, etc.)
-  msString = (msString + '00').substring(0, 3));
-
-module.exports.getTimezoneSeparatorFromString = (getTimezoneSeparatorFromString = function(string) {
-  let timezoneSeparator;
-  if (__guard__(string != null ? string.match(/-/) : undefined, x => x.length) === 1) {
-    return timezoneSeparator = '-';
-  } else if (__guard__(string != null ? string.match(/\+/) : undefined, x1 => x1.length) === 1) {
-    return timezoneSeparator = '+';
-  } else {
-    return timezoneSeparator = '';
-  }
-});
-
-function __guard__(value, transform) {
-  return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
 }
+
+function normalizeMillisecondsField(msString) {
+  // fix up milliseconds by padding zeros and/or truncating (5 --> 500, 50 --> 500, 54321 --> 543, etc.)
+  return msString = (msString + '00').substring(0, 3);
+}
+
+function getTimezoneSeparatorFromString(string) {
+  if (string != null) {
+    let matches = string.match(/-/);
+    if (matches && matches.length === 1) {
+      return '-';
+    }
+    matches = string.match(/\+/);
+    if (matches && matches.length === 1) {
+      return '+';
+    }
+  }
+  return '';
+}
+
+module.exports = { removeNulls, numerical_sort, isNull, typeIsArray, allTrue, anyTrue, jsDate, normalizeMillisecondsFieldInString, normalizeMillisecondsField, getTimezoneSeparatorFromString };
