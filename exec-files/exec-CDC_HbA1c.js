af1abdf91ae8eaa6e737cdeff28aa5558cf3612a
tylernoblett <tyler.noblett@outlook.com>
2020-12-11 13:07:01 -0500

    modify folder structure to prepare for other codes

--- exec-files/exec-CDC_HbA1c.js
+++ exec-files/exec-CDC_HbA1c.js
@@ -1,9 +1,9 @@
 const cql = require('../src/cql');
-const measure = require('./CDC_HbA1c.json');
+const measure = require('../json-elm/CDC_HbA1c.json');
 const codes = require('../src/cql-code-service');
 const cqlfhir = require('cql-exec-fhir');
-const patients = require('./cdc-patients');
-const codeservice = require('./temp_code');
+const patients = require('../data/patients/cdc-patients');
+const codeservice = require('../data/codes/cdc-codes');
 
 const lib = new cql.Library(measure);
 const cservice = new codes.CodeService(codeservice);
