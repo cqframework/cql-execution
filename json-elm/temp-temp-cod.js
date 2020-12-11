927bf03b48c1963789f39620b6925eddaa55429e
tylernoblett <tyler.noblett@outlook.com>
2020-12-11 11:47:30 -0500

    James' CDC works and is mostly correct (WIP)

---
+++ json-elm/temp-temp-cod.js
@@ -0,0 +1,60 @@
+"2.16.840.1.113762.1.4.1138.731": {
+  "Telehealth Modifier": [
+    {
+      "version": "2020-09",
+      "system": "CPT",
+      "code": "GQ"
+    },
+    // update with real info
+  ]
+},
+"2.16.840.1.113883.3.464.1003.101.11.1155": {
+  "Telehealth POS": [
+    {
+      "version": "2020-09",
+      "system": "CPT",
+      "code": "98966" 
+    },
+    // update with real info
+  ]
+},
+"2.16.840.1.113883.3.464.1004.1115": {
+  "HbA1c Level Less Than 7.0": [
+    { 
+      "version": "2020-09",
+      "system": "CPT",
+      "code": "98912"
+    }
+    // update with real info
+  ]
+},
+"2.16.840.1.113883.3.464.1004.1976": {
+  "HbA1c Level Greater Than or Equal to 7.0 and Less Than 8.0": [
+    { 
+      "version": "2020-09",
+      "system": "CPT",
+      "code": "98913"
+    }
+    // update with real info
+  ]
+},
+"2.16.840.1.113883.3.464.1004.1761": {
+  "Hospice Encounter": [
+    { 
+      "version": "2020-09",
+      "system": "CPT",
+      "code": "98914"
+    },
+    // update with real info
+  ]
+},
+"2.16.840.1.113883.3.464.1004.1531": {
+  "Frailty Diagnosis": [
+    {
+      "version": "2020-09",
+      "system": "CPT",
+      "code": "98915"
+    }
+    // update with real info
+  ]
+}
