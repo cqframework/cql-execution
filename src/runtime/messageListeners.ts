3f2b51f322b0ff6e0d4e671cab2cee362d46693b
mgramigna <mgramigna@mitre.org>
2022-02-21 10:05:34 -0500

    Properly mark code as type string in messageListeners.ts

--- src/runtime/messageListeners.ts
+++ src/runtime/messageListeners.ts
@@ -7,7 +7,7 @@ export class NullMessageListener {
 export class ConsoleMessageListener {
   constructor(public logSourceOnTrace = false) {}
 
-  onMessage(source: any, code: any, severity: string, message: string) {
+  onMessage(source: any, code: string, severity: string, message: string) {
     // eslint-disable-next-line no-console
     const print = severity === 'Error' ? console.error : console.log;
     let content = `${severity}: [${code}] ${message}`;
