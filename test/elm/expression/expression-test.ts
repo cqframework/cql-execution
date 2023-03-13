20693f9f6cd7ae78b8822b59abee51e476f75634
mgramigna <mgramigna@mitre.org>
2023-03-13 14:04:10 -0400

    Add more tests and use regex for specific error message parsing

--- test/elm/expression/expression-test.ts
+++ test/elm/expression/expression-test.ts
@@ -1,6 +1,8 @@
-import { Expression } from '../../../src/cql';
+import { fail } from 'assert';
 import should from 'should';
+import { Expression } from '../../../src/cql';
 import setup from '../../setup';
+import { AnnotatedError } from '../../../src/util/customErrors';
 const data = require('./data');
 
 class FailingExpression extends Expression {
@@ -18,22 +20,58 @@ describe('Expression', () => {
     setup(this, data);
   });
 
-  it('execute should throw error with localId, libraryIdentifier, and class name on invalid elm in Patient context', function () {
+  it('should throw annotated error with localId, libraryIdentifier, and class name on invalid elm in Patient context fully formatted during execution failure', function () {
     const testExpression = new FailingExpression({
       localId: 'test'
     });
 
     should(() => testExpression.execute(this.ctx)).throw(
-      'Expression FailingExpression from CQL Library: TestSnippet|1 with ELM local id: test caused the following error during execution: Execution Failed!'
+      'Encountered unexpected error during execution.\n\n\tError Message:\tExecution Failed!\n\tCQL Library:\tTestSnippet|1\n\tExpression:\tFailingExpression\n\tELM Local ID:\ttest\n'
     );
   });
 
-  it('execute should throw error with localId, libraryIdentifier, and class name on invalid elm in child context', function () {
+  it('should throw annotated error with localId, libraryIdentifier, and class name on invalid elm in child context during execution failure', function () {
     const testExpression = new FailingExpression({
       localId: 'test-nested'
     });
-    should(() => testExpression.execute(this.ctx.childContext())).throw(
-      'Expression FailingExpression from CQL Library: TestSnippet|1 with ELM local id: test-nested caused the following error during execution: Execution Failed!'
-    );
+
+    try {
+      testExpression.execute(this.ctx.childContext());
+      fail('test should have thrown an error before reaching this statement');
+    } catch (e: any) {
+      should(e).be.instanceOf(AnnotatedError);
+      should(e.message).containEql('Error Message:\tExecution Failed!');
+      should(e.message).containEql('CQL Library:\tTestSnippet|1');
+      should(e.message).containEql('Expression:\tFailingExpression');
+      should(e.message).containEql('ELM Local ID:\ttest-nested');
+    }
+  });
+
+  it('should throw annotated error with localId, libraryIdentifier, and class name on invalid elm in child context during execution failure', function () {
+    const expressionNoLocalId = new FailingExpression({});
+
+    try {
+      expressionNoLocalId.execute(this.ctx.childContext());
+      fail('test should have thrown an error before reaching this statement');
+    } catch (e: any) {
+      should(e).be.instanceOf(AnnotatedError);
+      should(e.message).not.containEql('ELM Local ID:');
+    }
+  });
+
+  it('should report "(unknown)" library for annotated error when parent of context is null', function () {
+    const testExpression = new FailingExpression({
+      localId: 'test'
+    });
+
+    try {
+      const childCtx = this.ctx.childContext();
+      childCtx.parent = null;
+      testExpression.execute(childCtx);
+      fail('test should have thrown an error before reaching this statement');
+    } catch (e: any) {
+      should(e).be.instanceOf(AnnotatedError);
+      should(e.message).containEql('CQL Library:\t(unknown)');
+    }
   });
 });
