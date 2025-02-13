96882591f342ec2bf857503c35de498916b55638
sorliog <sorliog@clemson.edu>
2025-02-13 17:36:54 -0500

    Built cql4browsers.js

--- examples/browser/cql4browsers.js
+++ examples/browser/cql4browsers.js
@@ -409,8 +409,16 @@ class ValueSet {
      * @returns {Code[]} An array of unique `Code` objects.
      */
     expand() {
-        const results = this.codes.map(code => code).filter((val, index, arr) => arr.indexOf(val) == index);
-        return results;
+        const expanded = [];
+        this.codes.forEach(code => {
+            const foundUniqueCode = expanded.find(uniqueCode => {
+                return uniqueCode.code === code.code && uniqueCode.system == code.system && uniqueCode.version == code.version && uniqueCode.display == code.display;
+            });
+            if (!foundUniqueCode) {
+                expanded.push(code);
+            }
+        });
+        return expanded;
     }
 }
 exports.ValueSet = ValueSet;
