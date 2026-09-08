7025a1303ceb340152f4069c4751fc503b2bc23d
Dylan Hall <dehall@mitre.org>
2026-09-08 14:11:25 -0400

    Add scale field and support to Decimal class

--- src/datatypes/decimal.ts
+++ src/datatypes/decimal.ts
@@ -11,19 +11,30 @@ export type DecimalInput = Decimal | string | number | bigint;
 
 export type DecimalRoundingMode = DecimalJS.Rounding;
 
-const MIN_PRECISION_VALUE = CQLDecimalJS.pow(10, -8);
-
 const CQL_IMPLICIT_SCALE = 8;
 const CQL_IMPLICIT_ROUNDING = CQLDecimalJS.ROUND_HALF_UP;
+const TRUNCATE_TO_PRECISION = CQLDecimalJS.ROUND_DOWN;
 
 export class Decimal {
-  private value: DecimalJS;
+  private readonly value: DecimalJS;
+  public readonly scale: number;
 
-  private constructor(value: string | number | bigint | DecimalJS) {
+  private constructor(value: string | number | bigint | DecimalJS, scale?: number) {
     this.value = new CQLDecimalJS(value);
     if (!this.value.isFinite()) {
       throw new Error('Cannot create a decimal with a non-finite value');
     }
+
+    if (scale == null) {
+      scale = determineScale(value, this.value);
+    } else if (!Number.isInteger(scale) || scale < 0) {
+      throw new RangeError('Decimal scale must be a non-negative integer');
+    } else if (scale < this.value.decimalPlaces()) {
+      // scale and value both provided, but the value has more decimal places,
+      // so apply the scale to the value to ensure internal consistency
+      this.value = this.value.toDecimalPlaces(scale, CQL_IMPLICIT_ROUNDING);
+    }
+    this.scale = scale;
   }
 
   static from(value: DecimalInput) {
@@ -39,37 +50,82 @@ export class Decimal {
   }
 
   normalized() {
-    if (this.value.decimalPlaces() <= CQL_IMPLICIT_SCALE) {
+    if (this.scale <= CQL_IMPLICIT_SCALE) {
       return this;
     }
-    return this.setScale(CQL_IMPLICIT_SCALE, CQL_IMPLICIT_ROUNDING);
+    return this.withScale(CQL_IMPLICIT_SCALE);
   }
 
   // Helper function to reduce repeated boilerplate.
   // Apply the given function with the given operand, and wrap the result in a Decimal.
-  private applyWrapper(operation: (value: any) => DecimalJS, other: DecimalInput): Decimal {
-    const operand = other instanceof Decimal ? other.value : other;
+  // A function to set an appropriate scale based on the scales of the inputs may also be provided.
+  private applyWrapper(
+    operation: (value: DecimalJS) => DecimalJS,
+    other: DecimalInput,
+    scaleLogic?: (scaleL: number, scaleR: number) => number
+  ): Decimal {
+    const decimalOther = Decimal.from(other);
 
-    return new Decimal(operation.call(this.value, operand));
+    const unscaledResult = new Decimal(operation.call(this.value, decimalOther.value));
+
+    if (scaleLogic) {
+      const targetScale = scaleLogic.call(null, this.scale, decimalOther.scale);
+      return unscaledResult.withScale(targetScale);
+    }
+    return unscaledResult;
   }
 
   add(other: DecimalInput): Decimal {
-    return this.applyWrapper(this.value.add, other);
+    // scale logic: max(scale(left), scale(right))
+    return this.applyWrapper(this.value.add, other, Math.max);
   }
 
   subtract(other: DecimalInput): Decimal {
-    return this.applyWrapper(this.value.minus, other);
+    // scale logic: max(scale(left), scale(right))
+    return this.applyWrapper(this.value.minus, other, Math.max);
   }
 
   multiplyBy(other: DecimalInput): Decimal {
-    return this.applyWrapper(this.value.times, other);
+    const scaleLogic = (l: number, r: number) => Math.min(l + r, CQL_IMPLICIT_SCALE);
+    return this.applyWrapper(this.value.times, other, scaleLogic);
   }
 
   divideBy(other: DecimalInput): Decimal {
-    if (Decimal.from(other).equals(0)) {
+    const decimalOther = Decimal.from(other);
+    if (decimalOther.equals(0)) {
       throw new RangeError('Cannot divide a decimal by zero');
     }
-    return this.applyWrapper(this.value.dividedBy, other);
+    // division scaling is more complex, depends on whether the actual result can be represented exactly
+    const unscaledResult = this.applyWrapper(this.value.dividedBy, decimalOther);
+    const unscaledDecimalPlaces = unscaledResult.value.decimalPlaces();
+    if (unscaledDecimalPlaces > CQL_IMPLICIT_SCALE) {
+      // it either doesn't terminate, or terminates with more than 8 digits, eg:
+      // 1/3 = 0.33333333...
+      // 1/512 = 0.001953125 (9 digits)
+      return unscaledResult.withScale(CQL_IMPLICIT_SCALE);
+    }
+
+    // multiplication uses min(scale(l) + scale(r), cql_max_scale)
+    // division is the inverse of multiplication, so we'll define a lower bound "preferred scale" as the inverse:
+    // max(scale(l)-scale(r), 0)
+    const preferredScale = Math.max(this.scale - decimalOther.scale, 0);
+
+    // examples:
+    // |               | Preferred |  Expected  |
+    // | Expression    |  scale    |   result   |
+    // | ------------- | --------: | ---------: |
+    // | 4.0 / 2       |         1 |        2.0 |
+    // | 9.9 / 3.0     |         0 |        3.3 |
+    // | 1.0 / 2.0     |         0 |        0.5 |
+    // | 1.0 / 4.0     |         0 |       0.25 |
+    // | 1.0 / 8.0     |         0 |      0.125 |
+    // | 1.0 / 3.0     |         0 | 0.33333333 |
+    // | 2.0 / 3.0     |         0 | 0.66666667 |
+    // | 1.00000 / 2.0 |         4 |     0.5000 |
+    // | 1.0 / 1.00000 |         0 |          1 | (as Decimal, scale 0)
+    // | 1.00000 / 1.0 |         4 |     1.0000 |
+
+    return unscaledResult.withMinimumScale(preferredScale);
   }
 
   modulo(other: DecimalInput) {
@@ -106,32 +162,59 @@ export class Decimal {
     return this.compareTo(other) === 0;
   }
 
+  equivalent(other: DecimalInput) {
+    // For decimals, equivalent means the values are the same
+    // with the comparison done on values rounded to
+    // the precision of the least precise operand;
+    // trailing zeroes after the decimal are ignored in determining precision
+    // for equivalent comparison.
+
+    // Because it ignores trailing zeros, we use decimal.js .decimalPlaces() instead of this.scale
+    const decimalOther = Decimal.from(other);
+    const lessPreciseScale = Math.min(
+      this.value.decimalPlaces(),
+      decimalOther.value.decimalPlaces()
+    );
+
+    return this.withScale(lessPreciseScale).equals(decimalOther.withScale(lessPreciseScale));
+  }
+
   successor() {
-    // TODO: successor should be based on current precision
-    // For Decimal, successor is equivalent to adding 1 * the precision of the argument.
-    return new Decimal(this.value.add(MIN_PRECISION_VALUE));
+    // "For Decimal, successor is equivalent to adding 1 * the precision of the argument."
+    // note that this is not 1 * Precision(this), since Precision is a number 0-8
+    const precision = Decimal.from(0.1).power(this.scale);
+    return this.add(precision);
   }
 
   predecessor() {
-    // TODO: predecessor should be based on current precision
-    // For Decimal, predecessor is equivalent to subtracting 1 * the precision of the argument.
-    return new Decimal(this.value.minus(MIN_PRECISION_VALUE));
+    // "For Decimal, predecessor is equivalent to subtracting 1 * the precision of the argument."
+    // note that this is not literally 1 * Precision(this), since Precision is a number 0-8
+    const precision = Decimal.from(0.1).power(this.scale);
+    return this.subtract(precision);
   }
 
   negate() {
-    return new Decimal(this.value.neg());
+    return new Decimal(this.value.neg(), this.scale);
   }
 
   abs() {
-    return new Decimal(this.value.abs());
+    return new Decimal(this.value.abs(), this.scale);
   }
 
   truncate(): number {
     return this.value.truncated().toNumber();
   }
 
-  truncated(): Decimal {
-    return new Decimal(this.value.truncated());
+  truncated(scale?: number): Decimal {
+    // specifying a scale here allows for "truncating to a precision"
+    // this is currently used in Interval.expand
+
+    if (!scale) {
+      // undefined or 0 both mean truncated to an integer
+      return new Decimal(this.truncate(), 0);
+    }
+
+    return this.withScale(scale, TRUNCATE_TO_PRECISION);
   }
 
   ceil(): number {
@@ -151,7 +234,7 @@ export class Decimal {
   }
 
   sqrt() {
-    return new Decimal(this.value.sqrt());
+    return new Decimal(this.value.sqrt()).withMinimumScale(this.scale);
   }
 
   ln() {
@@ -163,7 +246,7 @@ export class Decimal {
   }
 
   log(base: DecimalInput) {
-    return this.applyWrapper(this.value.log, base);
+    return this.applyWrapper(this.value.log, base).withMinimumScale(this.scale);
   }
 
   round(scale: number) {
@@ -173,29 +256,33 @@ export class Decimal {
     // ROUND_HALF_CEIL "Rounds towards nearest neighbour. If equidistant, rounds towards Infinity"
     // rounds 0.5 -> 1.0, -0.5 -> 0.0
     // https://mikemcl.github.io/decimal.js/#modes
-    return this.setScale(scale, CQLDecimalJS.ROUND_HALF_CEIL);
+    return this.withScale(scale, CQL_IMPLICIT_ROUNDING);
   }
 
-  setScale(scale: number, roundingMode: DecimalRoundingMode = CQLDecimalJS.ROUND_DOWN) {
+  withScale(scale: number, roundingMode: DecimalRoundingMode = CQL_IMPLICIT_ROUNDING) {
     if (!Number.isInteger(scale) || scale < 0) {
       throw new RangeError('Decimal scale must be a non-negative integer');
     }
 
-    return new Decimal(this.value.toDecimalPlaces(scale, roundingMode));
+    return new Decimal(this.value.toDecimalPlaces(scale, roundingMode), scale);
   }
 
-  toInteger() {
-    // note that this is permissive and converts non-integral values
-    return this.truncate();
+  // Some functions would prefer a given scale for the result but will allow a greater one
+  // if needed to represent the value.
+  // Eg, 1.0 / 1.0 and 1.0 / 3.0 both have exactly the same input scales, but expect different output scales.
+  withMinimumScale(scale: number, roundingMode: DecimalRoundingMode = CQL_IMPLICIT_ROUNDING) {
+    if (this.scale > scale) {
+      return this;
+    }
+    return this.withScale(scale, roundingMode);
+  }
+
+  withoutTrailingZeros() {
+    return this.withScale(this.value.decimalPlaces());
   }
 
   toNumber() {
     return this.value.toNumber();
-  }
-
-  toLong() {
-    // note that this is permissive and converts non-integral values
-    return BigInt(this.value.truncated().toString());
   }
 
   toString() {
@@ -207,12 +294,35 @@ export class Decimal {
     // (# means any number of digits, including none; 0 means a digit must appear)
     // a regex for this is -?\d+\.\d+
     // so Decimal.from(1).toString() --> "1.0"
-    const places = Math.max(1, this.value.decimalPlaces());
+    const places = Math.max(1, this.scale);
     return this.value.toFixed(places);
   }
 
   toJSON() {
-    return this.toString();
+    // The FHIR spec serializes `decimal` as a number, so we follow that convention here,
+    // but note the risk of loss of precision.
+    // https://hl7.org/fhir/json.html#primitive
+    return this.toNumber();
+  }
+}
+
+function determineScale(rawValue: string | number | bigint | DecimalJS, parsedValue?: DecimalJS) {
+  // decimal.js doesn't retain trailing zeros,
+  // so if provided a string we have to count the decimal places
+  if (typeof rawValue === 'string') {
+    const dpIndex = rawValue.indexOf('.');
+    if (dpIndex >= 0) {
+      return rawValue.length - dpIndex - 1;
+      // note this may be larger than our max scale; handle that in calling functions if necessary
+    } else {
+      return 0; // No decimal point found
+    }
+  } else {
+    // If not a string, fall back to parsing by decimal.js and use its decimalPlaces function
+    if (!parsedValue) {
+      parsedValue = new DecimalJS(rawValue);
+    }
+    return parsedValue.decimalPlaces();
   }
 }
 
